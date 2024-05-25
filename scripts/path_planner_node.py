81acf6f4bd175e4949317026ee9b5296fc8fd4f7
EtienneClr-Projects <etienne.clairis@etik.com>
2024-05-25 20:36:00 +0200

    move astar in a file

--- scripts/path_planner_node.py
+++ scripts/path_planner_node.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-from utils import dist_point_to_segment
-
 from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped, Quaternion
 from std_msgs.msg import Empty
 from tf.transformations import quaternion_from_euler
@@ -9,30 +7,10 @@ from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import OccupancyGrid, Path
 
 import numpy as np
-import rospy, queue, os, cv2, pickle
+import rospy
 from math import cos, sin, atan2, pi, sqrt
 
-class Etats():
-    def __init__(self, x, y, cost, liste_actions):
-        self.x = x
-        self.y = y
-        self.cost = cost
-        self.liste_actions = liste_actions
-
-    def __eq__(self, other):
-        if hash(self) == hash(other):
-            return True
-        return False
-    
-    def __hash__(self):
-        return hash((self.x, self.y))
-    
-    def __lt__(self, other):
-        return self.cost < other.cost
-    
-    def __str__(self):
-        return self.liste_actions
-
+from astar import astar
 
 class PathPlanner:
     def __init__(self):
@@ -54,7 +32,7 @@ class PathPlanner:
 
         self.is_goal_reached_sub = rospy.Subscriber("/is_goal_reached", Empty, self.receive_goal_reached)
         self.goal_reached = True
-        self.goal_pose = np.array([4., -1., 0.])
+        self.goal_pose = np.array([-4., -1., 0.])
 
         # costmap
         self.costmap_size = 200  # points
@@ -134,94 +112,6 @@ class PathPlanner:
     def run(self):
         rospy.spin()
 
-    def astar(self, start, goal):
-        # distance euclidienne entre 2 points
-        def dist(pos1, pos2):
-            return np.linalg.norm(np.array(pos2) - np.array(pos1))
-        
-        # calcul de la distance par rapport au mur le plus proche
-        # on va chercher à s'en éloigner + recherche dans un rayon de 2 cases autours de la position cible
-        def distance_to_nearest_wall(pos):
-            x, y = pos
-            if self.obstacle_layer[y, x] >= 100:
-                return 0
-            distances = []
-            for i in range(max(0, x-2), min(self.costmap_size, x+3)):
-                for j in range(max(0, y-2), min(self.costmap_size, y+3)):
-                    if self.obstacle_layer[j, i] >= 100:
-                        distances.append(dist((x, y), (i, j)))
-            return min(distances) if distances else float('inf')
-
-        grid_start = (
-            int((start[0] - self.origin_x) / self.resolution + self.costmap_size // 2),
-            int((start[1] - self.origin_y) / self.resolution + self.costmap_size // 2)
-        )
-
-        grid_goal = (
-            int((goal[0] - self.origin_x) / self.resolution + self.costmap_size // 2),
-            int((goal[1] - self.origin_y) / self.resolution + self.costmap_size // 2)
-        )
-
-        state = Etats(grid_start[0], grid_start[1], 0, [])
-
-        # liste des actions possible pour le robot
-        actions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        # actions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (1, 1), (-1, 1), (1, -1)] # avec diagonales
-        # setup des listes
-        liste_chemin = queue.PriorityQueue()
-        chemin_deja_visite = set()
-        # première valeur dans la liste
-        liste_chemin.put((0, state))
-        while not liste_chemin.empty():
-            # on récupère le tuple en première position dans la liste
-            node = liste_chemin.get()
-            current_state = node[1]
-            # on vérifie si c'est un chemin déjà visité
-            if current_state in chemin_deja_visite:
-                continue
-            chemin_deja_visite.add(current_state)
-            for dx, dy in actions:
-                next_pos = (current_state.x + dx, current_state.y + dy)
-                # on vérifie si la prochaine position est dans la grille / costmap : sécurité
-                if not (0 <= next_pos[0] < self.costmap_size and 0 <= next_pos[1] < self.costmap_size):
-                    continue
-
-                # on vérifie si la position cible est un mur
-                if self.obstacle_layer[next_pos[1], next_pos[0]] >= 100:
-                    chemin_deja_visite.add(new_state)
-                    continue
-
-                # calcul du prochain coût pour la prochaine position
-                next_cost = current_state.cost + (sqrt(2) if (dx, dy) in [(1, 1), (1, -1), (-1, 1), (-1, -1)] else 1)
-                margin = 3  # marge de sécurité en nombre de cellules
-                distance_wall = distance_to_nearest_wall(next_pos)
-
-                # si on est trop proche d'un mur, la position ciblée prend un coup supplémentaire
-                if distance_wall < margin:
-                    next_cost += (margin - distance_wall) * 10
-
-                # on va chercher à éviter les changements de direction trop souvent
-                if current_state.liste_actions:
-                    last_action = current_state.liste_actions[-1]
-                    # on punie les cas où on doit effectuer un changement de direction par rapport à l'action précédente
-                    # tourner, même en diagonal, a un coût
-                    if (last_action[0], last_action[1]) != (dx, dy):
-                        next_cost += 2
-
-                new_state = Etats(next_pos[0], next_pos[1], next_cost, current_state.liste_actions + [(dx, dy)])
-
-                # on vérifie si c'est un chemin déjà visité
-                if new_state in chemin_deja_visite:
-                    continue
-
-                # on vérifie si c'est notre destination
-                if next_pos == grid_goal:
-                    return new_state.liste_actions
-                
-                # si aucune des conditions précédentes n'est remplie, on l'ajoute à notre liste des chemins à "étudier"
-                liste_chemin.put((next_cost + dist(next_pos, grid_goal), new_state))
-        return None
-    
     def publish_path(self, path):
         path_msg = Path()
         path_msg.header.stamp = rospy.Time.now()
@@ -252,9 +142,12 @@ class PathPlanner:
         self.path_pub.publish(path_msg)
 
     def convert_sequence_to_poses_path(self, path, start):
-        # path is a sequence of (x,y) movement, with x,y = -1, 0 or 1
-        # we convert the sequence to have only the keypoints of the movements
-        # for instance the sequence (1,0) (1,0) (1,0) (0,1) will be (3,0) (0,1)
+        """
+        path is a sequence of (x,y) movement, with x,y = -1, 0 or 1
+        we convert the sequence to have only the keypoints of the movements
+        for instance the sequence (1,0) (1,0) (1,0) (0,1) will be (3,0) (0,1)
+        and then we convert the keypoints to global positions relative to start
+        """
         path.append((0,0))
         path = np.array(path)
         last_move = np.array([0,0])
@@ -289,7 +182,7 @@ class PathPlanner:
         goal = (self.goal_pose[0], self.goal_pose[1])
 
 
-        movement_sequence = self.astar(start, goal)
+        movement_sequence = astar(start, goal, self.costmap_size, self.resolution, self.origin_x, self.origin_y, self.obstacle_layer)
         if movement_sequence is None:
             print("movement seq is None")
             return
