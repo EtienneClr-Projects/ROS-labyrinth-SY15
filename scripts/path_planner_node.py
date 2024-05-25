553675ebb887545ad21218166e4e45c2f541e49b
EtienneClr-Projects <etienne.clairis@etik.com>
2024-05-25 12:49:18 +0200

    auto find next goal with nearest seg

--- scripts/path_planner_node.py
+++ scripts/path_planner_node.py
@@ -296,6 +296,31 @@ class PathPlanner:
             poses_path.append(current)
 
         return poses_path
+    
+    def get_next_goal_on_path(self, poses_path):
+        """
+        Returns the next goal on the path
+        Find the nearest line on the path to the robot and returns the end of the line
+        """
+        def dist_point_to_segment(point, seg_start, seg_end):
+            x0, y0 = point
+            x1, y1 = seg_start
+            x2, y2 = seg_end
+            return abs((x2-x1)*(y1-y0) - (x1-x0)*(y2-y1)) / sqrt((x2-x1)**2 + (y2-y1)**2)
+
+        segs = []
+        for i in range(1, len(poses_path)):
+            segs.append((poses_path[i-1], poses_path[i]))
+
+        min_dist = float('inf')
+        nearest_seg = None
+        for seg in segs:
+            dist = dist_point_to_segment(self.current_pose[:2], seg[0], seg[1])
+            if dist < min_dist:
+                min_dist = dist
+                nearest_seg = seg
+
+        return nearest_seg[1]
 
     def planning_loop(self, _):
         def is_too_near(pos1, pos2):
@@ -315,7 +340,7 @@ class PathPlanner:
         print("convert seq")
         poses_path = self.convert_sequence_to_poses_path(movement_sequence, start)
 
-        goal = poses_path[1] # path[0] is the current position
+        goal = self.get_next_goal_on_path(poses_path)
 
         self.publish_path(poses_path)
         self.publish_goal(goal)
